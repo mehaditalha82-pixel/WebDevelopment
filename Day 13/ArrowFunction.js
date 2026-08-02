b9c87fec5270af6d7e528dd27f31e77eb17e609a
metalha516 <mehaditalha62@gmail.com>
2026-08-02 15:48:58 +0600

    Destructuring Array and Objects

--- Day 13/ArrowFunction.js
+++ Day 13/ArrowFunction.js
@@ -24,3 +24,19 @@ console.log(sum(10, 30));
 hi = name=> console.log("hi");
 
 hi();
+
+//arrow function dont have self this value;
+
+let student = {
+    name : "talha",
+    age : 20,
+    show : function(){
+        return `Name is ${this.name}, age is ${this.age}`;
+    },
+    showAge : ()=>{
+        return `Name is ${this.name}, age is ${this.age}`;
+    }
+}
+
+console.log(student.show());
+console.log(student.showAge());
---
+++ Day 13/Destructure.js
@@ -0,0 +1,22 @@
+let array = [10, 20, 30, 40, 50, 0, 70, 80, 90];
+
+let [first, sec, ,,,six] = array;
+
+console.log(first, sec, six);
+
+//Object Destructure 
+
+let student = {
+    name : "Talha",
+    age : 25,
+    Occupation : "Engineer",
+    Skills : {
+        programming : 90,
+        communication : 40,
+        publicSpeaking : 10,
+    }
+}
+
+const {name, Occupation : job, Skills : {programming}} = student;
+
+console.log(name, job, programming);
