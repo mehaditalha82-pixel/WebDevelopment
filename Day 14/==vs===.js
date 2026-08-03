b5022f23cd9dbcf0ab81979caac4258cc89babdd
metalha516 <mehaditalha62@gmail.com>
2026-08-03 15:07:14 +0600

    Loose Equality vs Strict Equality

---
+++ Day 14/==vs===.js
@@ -0,0 +1,15 @@
+// == -> loosy condition check 
+
+//for double == saying they are same, but we know they are different 
+if(5 == "5"){
+    console.log("Both are same");
+}else{
+    console.log("both are not same");
+}
+
+//for === giving the actual result 
+if(5 === "5"){
+    console.log("Both are same");
+}else{
+    console.log("both are not same");
+}
