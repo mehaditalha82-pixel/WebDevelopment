abe3f4baf438c398bc807d6d0f5a38d6ceab531d
metalha516 <mehaditalha62@gmail.com>
2026-08-03 15:59:18 +0600

    a++ and ++a

---
+++ Day 14/preincrement_postIncrement.js
@@ -0,0 +1,14 @@
+
+/* 
+x++ => use present value then increase by 1 
+++x => increase by 1 then use increased value  */
+
+let a = 5;
+console.log(a++);
+console.log(a);
+
+
+let b = 5;
+console.log(++b);
+console.log(b);
+
