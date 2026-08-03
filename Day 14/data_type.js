5c895e3c75320a702659870c2cd097d9bb9e3c1b
metalha516 <mehaditalha62@gmail.com>
2026-08-03 14:46:00 +0600

    Data_types

---
+++ Day 14/data_type.js
@@ -0,0 +1,21 @@
+//primitive Datatype 
+
+let x = "I am a good boy"  //<- string 
+let y = 90 //Number
+let isNum = true // boolean 
+let h = undefined //undefined 
+let j = null //null
+
+console.log(typeof y)
+
+//Non premitive data type 
+
+let arr = [10, 2, 19, 4, 59];
+let obj = {
+    name : "Talha",
+    age  : 20,
+    section : 'A',
+}
+
+console.log(typeof arr);
+console.log(typeof obj);
