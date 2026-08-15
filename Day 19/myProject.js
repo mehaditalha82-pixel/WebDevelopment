d19682749b19632cebe0056dd78c7b1194b916d3
metalha516 <mehaditalha62@gmail.com>
2026-08-16 05:31:35 +0600

    Type Script Type Creation

---
+++ Day 19/myProject.js
@@ -0,0 +1,6 @@
+"use strict";
+let student1 = {
+    name: "karim",
+    age: 20,
+};
+console.log(student1);
---
+++ Day 19/myProject.ts
@@ -0,0 +1,12 @@
+type Student = {
+    name : string,
+    age : number,
+    is_passed? : boolean
+}
+
+let student1 : Student = {
+        name : "karim",
+        age : 20,
+}
+
+console.log(student1)
